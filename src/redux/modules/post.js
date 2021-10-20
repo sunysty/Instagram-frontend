7461380babad4a2de42a4f387a857a8657eb3542
jeoungwoo <ajw2312@gmail.com>
2021-10-20 18:20:43 +0900

    변경내용 저장

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import 'moment';
 import moment from 'moment';
 import user from './user';
+import comment from './comment';
 
 // 목록 redux
 const SET_POST = 'SET_POST';
@@ -13,6 +14,8 @@ const ADD_POST = 'ADD_POST';
 const EDIT_POST = 'EDIT_POST';
 const DELETE_POST = 'DELETE_POST';
 // const LOADING = 'LOADING';
+const NEW_COMMENT = 'NEW_COMMENT';
+const OLD_COMMENT = 'OLD_COMMENT';
 
 const setPost = createAction(SET_POST, (post_list) => ({ post_list }));
 const addPost = createAction(ADD_POST, (post) => ({ post }));
@@ -22,6 +25,12 @@ const editPost = createAction(EDIT_POST, (post_id, post) => ({
 }));
 const deletePost = createAction(DELETE_POST, (userName) => ({ userName }));
 // const loading = createAction(LOADING, (post, post_id) => ({ post, post_id }));
+const newComment = createAction(NEW_COMMENT, (commentInfo) => ({
+  commentInfo,
+}));
+const oldComment = createAction(OLD_COMMENT, (commentInfo) => ({
+  commentInfo,
+}));
 
 const initialState = {
   list: [],
@@ -52,9 +61,6 @@ const addPostAX = (post) => {
     const options = {
       url: '/api/upload',
       method: 'POST',
-      headers: {
-        token: token,
-      },
       data: formData,
     };
     axios(options)
@@ -92,3 +98,168 @@ const addPostAX = (post) => {
     // };
   };
 };
+
+const getAllPostAX = (history) => {
+  return function (dispatch, getState) {
+    const options = {
+      url: '/api/post',
+      method: 'POST',
+    };
+    axios(options)
+      .then((res) => {
+        console.log(res.data);
+        let post_list = [];
+        // res로 받은 데이터 정리
+        for (let i = 0; i < res.data.post_list.length; i++) {
+          post_list.push({
+            post_id: res.data.post_list[i].post_id,
+            content: res.data.post_list[i].content,
+            image: res.data.post_list[i].image,
+            profile_image: res.data.post_list[i].profile_image,
+            comments: res.data.post_list[i].comments,
+          });
+        }
+        // redux 업데이트
+        dispatch(setPost(post_list));
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.res) {
+          window.alert('getAllPost 에러');
+        }
+      });
+  };
+};
+
+// const getMyPostAX = (history) => {
+//   return function (dispatch, getState) {
+//     const res = res.post_list;
+//     let post_list = [];
+//     for (let i = 0; i < res.length; i++) {
+//       post_list.push({
+//         image: res[i].file,
+//         name: res[i].userName,
+//         content: res[i].content
+//       });
+//     }
+//     dispatch(setPost(post_list));
+//   };
+// };
+
+const deletePostAX = (post_id) => {
+  return function (dispatch) {
+    const options = {
+      url: '/api/post',
+      method: 'DELETE',
+      data: {
+        post_id: post_id,
+      },
+    };
+    axios(options)
+      .then((res) => {
+        console.log(res);
+        // 삭제한 게시글의 post_id를 이용, redux 상태 업데이트
+        dispatch(deletePost(post_id));
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.res) {
+          window.alert('게시글 삭제 에러');
+        }
+      });
+  };
+};
+
+const editPostAX = (content, post_id) => {
+  return function (dispatch) {
+    const options = {
+      url: '/api/post',
+      method: 'PUT',
+      data: {
+        post_id: post_id,
+        content: content,
+      },
+    };
+    axios(options)
+      .then((res) => {
+        let post_list = {
+          post_id: post_id,
+          content: content,
+        };
+        dispatch(editPost(post_list));
+        // 수정한 게시글의 post_id, content를 묶어서 redux 상태 업데이트
+      })
+      .catch((err) => {
+        if (err.res) {
+          window.alert('게시글 수정 에러');
+        }
+      });
+  };
+};
+
+// reducer
+export default handleActions(
+  {
+    [SET_POST]: (state, action) =>
+      produce(state, (draft) => {
+        draft.list = action.payload.post_list;
+      }),
+
+    [ADD_POST]: (state, action) =>
+      produce(state, (draft) => {
+        draft.list.unshift(action.payload.post);
+      }),
+
+    [DELETE_POST]: (state, action) =>
+      produce(state, (draft) => {
+        let new_post_list = draft.list.filter((e) => {
+          if (e.post_id !== action.payload.post) {
+            return e;
+          }
+        });
+        draft.list = new_post_list;
+      }),
+
+    [EDIT_POST]: (state, action) =>
+      produce(state, (draft) => {
+        // post_list 게시글 데이터중 방금 수정한 게시글과 post_id를 비교하여
+        let idx = draft.list.findIndex(
+          (p) => p.post_id === action.payload.post.post_id
+        );
+
+        // 그 인덱스의 content를 수정된 content로 바꿔준다.
+        draft.list[idx].content = action.payload.post.content;
+      }),
+
+    // Post에서도 댓글 게시가 가능해서 넣음
+    [NEW_COMMENT]: (state, action) =>
+      produce(state, (draft) => {
+        let idx = draft.list.findIndex(
+          (p) => p.post_id === action.payload.commentInfo
+        );
+        draft.list[idx].comments.push('add');
+      }),
+
+    [OLD_COMMENT]: (state, action) =>
+      produce(state, (draft) => {
+        let idx = draft.list.findIndex(
+          (p) => p.post_id === action.payload.commentInfo
+        );
+        draft.list[idx].comments.pop();
+      }),
+  },
+  initialState
+);
+
+const actionCreators = {
+  setPost,
+  addPostAX,
+  getAllPostAX,
+  // getMyPostAX,
+  deletePostAX,
+  editPostAX,
+  newComment,
+  oldComment,
+};
+
+export { actionCreators }
